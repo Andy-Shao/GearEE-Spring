49bb897c71c5bd273232293153c16f4d7d8d1cd4
Andy-Shao <hj13035952619@126.com>
2016-03-09 14:25:21 +0800

    adjust coding

--- src/main/java/com/github/andyshaox/spring/jdbc/SpringJdbcDaoFactory.java
+++ src/main/java/com/github/andyshaox/spring/jdbc/SpringJdbcDaoFactory.java
@@ -1,12 +1,15 @@
 package com.github.andyshaox.spring.jdbc;
 
+import java.io.IOException;
 import java.lang.reflect.Proxy;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.github.andyshao.asm.Version;
 import com.github.andyshao.reflect.ClassOperation;
 import com.github.andyshaox.jdbc.Dao;
 import com.github.andyshaox.jdbc.DaoFactory;
+import com.github.andyshaox.jdbc.JdbcProcessException;
 
 /**
  * 
@@ -23,13 +26,20 @@ public class SpringJdbcDaoFactory implements DaoFactory {
     @Override
     public Object getDao(final Dao dao) {
         // TODO Auto-generated method stub
-        Set<Class<?>> set = new HashSet<>();
-        ClassOperation.superGetInterfaces(dao.getDefineClass() , set);
-        return Proxy.newProxyInstance(dao.getDefineClass().getClassLoader() , set.toArray(new Class<?>[set.size()]) ,
-            (target , method , args) -> {
-                if (!dao.getSqls().contains(method)) return null;
-                return null;
-            });
+        if (!dao.getDefineClass().isInterface()) throw new JdbcProcessException("Defining class should be a interface");
+        try {
+            final Object entity = ClassOperation.newInstanceForInterface(dao.getDefineClass() ,
+                dao.getDefineClass() + "Entity" , false , Version.V1_8);
+            final Set<Class<?>> set = new HashSet<>();
+            ClassOperation.superGetInterfaces(dao.getDefineClass() , set);
+            return Proxy.newProxyInstance(dao.getDefineClass().getClassLoader() ,
+                set.toArray(new Class<?>[set.size()]) , (target , method , args) -> {
+                    if (!dao.getSqls().containsKey(method)) return method.invoke(entity , args);
+                    return null;
+                });
+        } catch (IOException e) {
+            throw new JdbcProcessException("Cannot build interface entity" , e);
+        }
     }
 
 }
